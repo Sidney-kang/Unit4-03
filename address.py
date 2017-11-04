ce355a0ebda5cb96308f3a669b319647f35f8056
Sidney <sidney.kang@ocsbstudent.ca>
2017-11-04 16:08:30 -0400

    Edited version: added a new function

--- address.py
+++ address.py
@@ -7,21 +7,34 @@
 def print_address(apt_number, st_address, city, province, postal_code):
     if apt_number == "": 
        print(st_address + ", " + city + ", " + province + ", " + postal_code)   
+    elif apt_number == "redo":
+       print("Please type 'yes' or 'no'")
+       setup()
+       return 
     else:
        print(apt_number + ", " + st_address + ", " + city + ", " + province + ", " + postal_code) 
+def setup():      
+    apt_number = raw_input("Would you like to enter your apartment number (yes or no): ")
+    if apt_number == "yes":
+       apt_number = raw_input("Please enter your apartment number: ")
+       st_address = raw_input("Please enter your street address: ")
+       city = raw_input("Please enter the city you live in: ")
+       province = raw_input("Please enter the province you live in: ")
+       postal_code = raw_input("Please enter you postal code: ")
+       print_address(apt_number, st_address, city, province, postal_code)
+    elif apt_number == "no":
+       apt_number = ""
+       st_address = raw_input("Please enter your street address: ")
+       city = raw_input("Please enter the city you live in: ")
+       province = raw_input("Please enter the province you live in: ")
+       postal_code = raw_input("Please enter you postal code: ")
+       print_address(apt_number, st_address, city, province, postal_code)  
+    else: 
+       apt_number = "redo"
+       st_address = ""
+       city = ""
+       province = ""
+       postal_code = ""
+       print_address(apt_number, st_address, city, province, postal_code)
        
-apt_number = raw_input("Would you like to enter your apartment number (yes or no): ")
-if apt_number == "yes":
-   apt_number = raw_input("Please enter your apartment number: ")
-   st_address = raw_input("Please enter your street address: ")
-   city = raw_input("Please enter the city you live in: ")
-   province = raw_input("Please enter the province you live in: ")
-   postal_code = raw_input("Please enter you postal code: ")
-   print_address(apt_number, st_address, city, province, postal_code)
-else: 
-   apt_number = ""
-   st_address = raw_input("Please enter your street address: ")
-   city = raw_input("Please enter the city you live in: ")
-   province = raw_input("Please enter the province you live in: ")
-   postal_code = raw_input("Please enter you postal code: ")
-   print_address(apt_number, st_address, city, province, postal_code)
+setup()
