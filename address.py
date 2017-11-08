3a3a66f0d0819e160d59cabc2ce1782eed27962e
Sidney <sidney.kang@ocsbstudent.ca>
2017-11-08 11:31:20 -0500

    Edited version: made apartment number optional

--- address.py
+++ address.py
@@ -4,7 +4,7 @@
 # Daily Assignment - Unit4-03
 # This program accepts an address and formats it nicely
 
-def print_address(apt_number, st_address, city, province, postal_code):
+def print_address(st_address, city, province, postal_code, apt_number = ""):
     if apt_number == "": 
        print(st_address + ", " + city + ", " + province + ", " + postal_code)   
     elif apt_number == "redo":
@@ -13,6 +13,7 @@ def print_address(apt_number, st_address, city, province, postal_code):
        return 
     else:
        print(apt_number + ", " + st_address + ", " + city + ", " + province + ", " + postal_code) 
+       
 def setup():      
     apt_number = raw_input("Would you like to enter your apartment number (yes or no): ")
     if apt_number == "yes":
@@ -28,13 +29,6 @@ def setup():
        city = raw_input("Please enter the city you live in: ")
        province = raw_input("Please enter the province you live in: ")
        postal_code = raw_input("Please enter you postal code: ")
-       print_address(apt_number, st_address, city, province, postal_code)  
-    else: 
-       apt_number = "redo"
-       st_address = ""
-       city = ""
-       province = ""
-       postal_code = ""
-       print_address(apt_number, st_address, city, province, postal_code)
+       print_address(st_address, city, province, postal_code)  
        
 setup()
